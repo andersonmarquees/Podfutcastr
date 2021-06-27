ddf82bd93b881b67bb5ee7f95a42a3238434a595
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-06-27 17:31:00 -0300

    page home and styles

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,41 @@
 import styles from "./styles.module.scss";
 
 export function Player(): JSX.Element {
-  return <div className={styles.playerContainer}></div>;
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="playing now" />
+        <strong>Playing now</strong>
+      </header>
+      <div className={styles.emptyPlayer}>
+        <strong>Select a podcast to listen</strong>
+      </div>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider} />
+          </div>
+          <span>00:00</span>
+        </div>
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="shuffle" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="previous button" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="play" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="play next" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="repeat button" />
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
-export default function Home(): JSX.Element {
-  return <h1>index</h1>;
+export default function Home(props): JSX.Element {
+  return (
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = fetch("http://localhost:3333/episodes");
+  const data = await (await response).json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
